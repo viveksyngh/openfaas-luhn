20d8577c5f6cb75f2b980c811add2b98d48a4a8a
Vivek Singh <vivekkmr45@yahoo.in>
2017-12-15 08:05:42 +0000

    first commit

---
+++ luhn/handler.go
@@ -0,0 +1,51 @@
+package function
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+// Handle a serverless request
+func Handle(req []byte) string {
+	return fmt.Sprintf(strconv.FormatBool(Valid(string(req))))
+}
+
+//Valid checks if a string given follows luhn's algorithm or not.
+func Valid(number string) bool {
+
+	if len(number) <= 1 {
+		return false
+	} else if len(number) == 2 && strings.HasPrefix(number, " ") {
+		return false
+	}
+
+	number = strings.Replace(number, " ", "", -1)
+	digits := []int{}
+
+	for _, char := range number {
+		digit, err := strconv.Atoi(string(char))
+		if err != nil {
+			return false
+		}
+		digits = append(digits, digit)
+	}
+
+	for i := len(digits) - 2; i >= 0; i = i - 2 {
+		if digits[i]*2 > 9 {
+			digits[i] = digits[i]*2 - 9
+		} else {
+			digits[i] = digits[i] * 2
+		}
+	}
+
+	sum := 0
+	for _, digit := range digits {
+		sum += digit
+	}
+
+	if sum%10 == 0 {
+		return true
+	}
+	return false
+}
